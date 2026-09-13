c16db5252a604528779c0cbbfe2b33ecfcf7f87f
parsaeianh <hamidreza.p051@gmail.com>
2026-09-13 15:29:04 +0330

    Initial Catalog Health app

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,11 @@
+import type { LoaderFunctionArgs } from "react-router";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await authenticate.admin(request);
+  return null;
+};
+
+export default function Index() {
+  return <s-page heading="Catalog Health" />;
+}
